cfcf2e72ed856d0f5558a3b44a113a1eff3065d1
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-12 00:17:11 +0200

    chore: :art: Add changes to import modules.

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from app.models.models import Biblioteca, Libro
+from app.views.views import Vista_Biblioteca
+from app.controllers.controllers import Controlador_Biblioteca
+
+__all__ = [
+    "Biblioteca","Libro", "Vista_Biblioteca", "Controlador_Biblioteca"
+]
