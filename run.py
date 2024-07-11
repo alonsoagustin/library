4b780e4cd0e0775099b4247aac74b67a2782e81a
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-11 22:32:45 +0200

    feat: :sparkles: Initialize the application.

---
+++ run.py
@@ -0,0 +1,35 @@
+from app.models.models import Biblioteca
+from app.views.views import Vista_Biblioteca
+from app.controllers.controllers import Controlador_Biblioteca
+
+if __name__ == "__main__":
+    
+    biblioteca = Biblioteca()
+    vista = Vista_Biblioteca()
+    controlador = Controlador_Biblioteca(biblioteca, vista)
+    
+
+    while True:
+        print("\nOpciones:")
+        print("1. Agregar libro")
+        print("2. Eliminar libro")
+        print("3. Prestar libro")
+        print("4. Devolver libro")
+        print("5. Mostrar libros disponibles")
+        print("6. Salir")
+        opcion = int(input("Seleccione una opción: "))
+
+        if opcion == 1:
+            controlador.agregar_libro()
+        elif opcion == 2:
+            controlador.eliminar_libro()
+        elif opcion == 3:
+            controlador.prestar_libro()
+        elif opcion == 4:
+            controlador.devolver_libro()
+        elif opcion == 5:
+            controlador.mostrar_libros_disponibles()
+        elif opcion == 6:
+            break
+        else:
+            print("Opción no válida. Por favor, intente de nuevo.")
